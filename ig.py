8b66344d9e0de104223effe0aa3c440b7604cb5c
reyvand <reyvand@icloud.com>
2018-07-15 07:44:12 +0700

    Add feature load cookie from file

--- ig.py
+++ ig.py
@@ -13,16 +13,21 @@ def save_file(url, filename, userdir):
 	with open(filename, 'wb') as f:
 		shutil.copyfileobj(raw.raw, f)
 
-def find_raw(urlcontent):
-	content = BeautifulSoup(r.get(urlcontent).text, 'html.parser')
+def find_raw(urlcontent, cookie=''):
+	if cookie != '':
+		print("Load cookie file from %s" %(cookie))
+		custom_header = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0', 'Cookie':open(cookie).read()}
+	else:
+		custom_header = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0'}
+	content = BeautifulSoup(r.get(urlcontent, headers=custom_header).text, 'html.parser')
 	sc = content.find_all('script')
 	for s in sc:
 		if 'window._sharedData = {' in s.text:
 			return json.loads(s.text.split(' = ')[1][:-1])
 			break
 
-def get_content(url):
-	content = find_raw(url)
+def get_content(url, cookie=''):
+	content = find_raw(url, cookie)
 	if 'edge_sidecar_to_children' in str(content):
 		c = content['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_sidecar_to_children']['edges']
 		for i in range(len(c)):
@@ -84,10 +89,14 @@ if __name__ == '__main__':
 	parser.add_argument('-i', '--info', dest="info", help="Retrieve data from given username")
 	parser.add_argument('-p', '--profile-pic', dest="pp", help="Save profile photo from given username")
 	parser.add_argument('-a', '--save-all', dest="all", help="Save all posts picture/video from given username")
+	parser.add_argument('-c', '--cookie', dest="cookie", help="Load cookie from file")
 	args = parser.parse_args()
 
 	if args.url != None:
-		get_content(args.url)
+		if args.cookie != None:
+			get_content(args.url, args.cookie)
+		else:
+			get_content(args.url)
 	elif args.info != None:
 		print(display_info(fetch_profile(args.info)))
 	elif args.pp != None:
